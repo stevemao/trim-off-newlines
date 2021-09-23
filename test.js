c77691de9130d5c0021301593a1c7e3b3b0ff5a7
Rich Trott <rtrott@gmail.com>
2021-09-22 18:30:24 -0700

    fix: remediate ReDOS further

--- test.js
+++ test.js
@@ -21,8 +21,71 @@ it('should trim off \\r\\n', function () {
 });
 
 it('should not be susceptible to exponential backtracking', function () {
+	var redosString = 'a';
+	var count = 1000;
+	while (count) {
+		redosString += '\r\n';
+		count--;
+	}
+	redosString += 'a';
+
+	var longerRedosString = redosString;
+	count = 1000;
+	while (count) {
+		longerRedosString += redosString;
+		count--;
+	}
+
 	var start = Date.now();
-	trimOffNewlines('a' + '\r\n'.repeat(1000) + 'a');
+	trimOffNewlines(redosString);
+	trimOffNewlines(longerRedosString);
 	var end = Date.now();
-	assert.ok(end - start < 1000, 'took too long, probably susceptible to ReDOS');
+	assert.ok(end - start < 1000, 'took too long, susceptible to ReDoS?');
+});
+
+it('should be performant on very long strings', function () {
+	var longOrdinaryString = 'aa';
+	var count = 27;
+	while (count) {
+		longOrdinaryString += longOrdinaryString;
+		count--;
+	}
+	assert.strictEqual(longOrdinaryString.length, 268435456);
+
+	var start = Date.now();
+	trimOffNewlines(longOrdinaryString);
+	var end = Date.now();
+	assert.ok(end - start < 1000, 'took too long, performance issue?');
+});
+
+it('should be performant in worst-case', function () {
+	// In the current algorithm, this is likely a worst-case:
+	// non-newline character followed by many newline characters.
+
+	this.timeout(10000);
+
+	var worstCaseString = '\r\n';
+	var count = 27;
+	while (count) {
+		worstCaseString += worstCaseString;
+		count--;
+	}
+	worstCaseString = 'a' + worstCaseString;
+	assert.strictEqual(worstCaseString.length, 268435457);
+	var start = Date.now();
+	trimOffNewlines(worstCaseString);
+	var end = Date.now();
+	assert.ok(end - start < 5000, 'worst case took too long, performance issue?');
+});
+
+it('should leave newlines in the middle of a string alone', function () {
+	assert.strictEqual(trimOffNewlines('Come on,\nFhqwhgads.'), 'Come on,\nFhqwhgads.');
+});
+
+it('should leave spaces at start and end alone', function () {
+	assert.strictEqual(trimOffNewlines(' fhqwhgads '), ' fhqwhgads ');
+});
+
+it('should return an empty string if there are only \\r and \\n', function () {
+	assert.strictEqual(trimOffNewlines('\r\n\r\r\n\n'), '');
 });
